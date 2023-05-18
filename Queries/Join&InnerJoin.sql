0b426c80fe767e83d7d4b7963f268e945bf2800e
tknbrk <124456340+tknbrk@users.noreply.github.com>
2023-05-18 21:34:35 +0300

    Delete Join&InnerJoin.sql

--- Queries/Join&InnerJoin.sql
+++
@@ -1,5 +0,0 @@
---Join and Inner Join
-
---Return the all result that the CategoryID is same in Produts and Categories table
-Select * from Products inner join Categories
-on Products.CategoryID = Categories.CategoryID
