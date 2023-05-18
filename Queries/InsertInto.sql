37270e3797bb8869f84a31ac29c642b896e7134e
tknbrk <124456340+tknbrk@users.noreply.github.com>
2023-05-18 22:50:30 +0300

    Add files via upload

---
+++ Queries/InsertInto.sql
@@ -0,0 +1,6 @@
+--Insert Into
+--Add a new row for Categories table
+insert into Categories (CategoryName,Description, Picture)
+values('Test', 'Test Description','3131321321321')
+
+insert into [Order Details] values(10248, 12, 12, 10,0)
